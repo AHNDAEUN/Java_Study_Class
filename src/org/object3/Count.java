ad78132124dcd9281cc8aa45d799c482d562aacd
ahndaeun <dean034@naver.com>
2022-07-13 17:49:08 +0900

    //

---
+++ src/org/object3/Count.java
@@ -0,0 +1,23 @@
+package org.object3;
+
+public class Count {
+
+	public void hap(int num1, int num2) {
+		System.out.println(num1+num2);
+		
+	}
+	
+	public void hap (int num1, double num2) {
+		System.out.println(num1*num2);
+	
+}
+	public void hap(int num1, int num2,int num3) {
+		
+	
+}
+	public int hap(int num2, int num1) {
+		//리턴타입과 상관없이 매개변수와 타입의 구성이 달라야함 
+		//리턴타입만 바꾸면 에러 발생
+	}
+	
+	}
---
+++ src/org/object3/Object3Main2.java
@@ -0,0 +1,11 @@
+package org.object3;
+
+public class Object3Main2 {
+public static void main(String[] args) {
+	
+	Count count = new Count();
+	
+	count.hap(10, 20);
+	count.hap2(10, 3.2);
+}
+}

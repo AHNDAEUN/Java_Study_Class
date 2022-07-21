28e9ddee0caef67680eaa2b618bf673270651da7
ahndaeun <dean034@naver.com>
2022-07-21 10:55:11 +0900

    7월21일 Weather 예제

--- src/org/lang/list_ex1/WeatherController.java
+++ src/org/lang/list_ex1/WeatherController.java
@@ -1,9 +1,10 @@
 package org.lang.list_ex1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WeatherController {
-	//WeatherController 
+	//WeatherController  
 	//
 	//start라는 메서드 하나 있음
 	//1번 누르면 날씨정보 초기화 - 호출(실행)= init메서드 호출 
@@ -13,11 +14,93 @@ public class WeatherController {
 	//5번 지역정보 삭제 = 해당정보 삭제 없으면 "없는 지역입니다." 메세지 노출
 	//6번 프로그램 종료 6번 누를때까지 메세지 반복 
 	
+	private WeatherService ws;
+	private Scanner sc;
+	private WeatherView wv;
+	
+	
+	//결합도가 강하다(높다) : 자체에서 객체 생성
+	//결합도가 느슨하다(낮다) : 매개변수를 통해서 만들어진 객체를 주입	
+	//                   : 생성자의 매개변수, 
+	
+	
+	public WeatherController() {
+		//ws =new WeatherService(); //내가 사용해야 하는 (의존하는) 메서드를 직접 선언할 수 있음
+		
+		this.ws = new WeatherService();
+		this.wv = new WeatherView();
+		this.sc = new Scanner(System.in);
+		
+	}
 	
 	public void start() {
 		
 		
-		ArrayList<CityDTO> ar = new ArrayList<>();
+		ArrayList<CityDTO> ar = new ArrayList<>(); 
+			boolean check = true;
+			
+			while (check) {
+				
+				System.out.println("1. 날씨 초기화");
+				System.out.println("2. 날씨 정보출력");
+				System.out.println("3. 지역날씨출력");
+				System.out.println("4. 지역정보추가");
+				System.out.println("5. 지역정보삭제");
+				System.out.println("6. 종     료");
+				
+				int select =sc.nextInt();
+				
+				if (select==1 ) {
+					
+				   ws.init(ar); // 초기화 하자
+				} else if (select==2 ){
+				
+					wv.view(ar);
+				}else if (select==3 ){
+					
+					// 1. 지역찾기 - 출력 return
+					 CityDTO cityDTO = ws.find(ar);
+					 if(cityDTO != null) {
+						wv.view(cityDTO);
+						 
+					 }else {
+						 wv.view(" 잘못된 도시명 ");
+					 }
+					 
+					}else if (select==4 ){
+						
+						boolean result = ws.add(ar);
+						String message="추가실패";
+						if (result){//result 자체가 투르펄스임
+						message ="추가 성공";	
+						
+						} 
+						
+						wv.view(message);
+
+					}else if (select==5 ){
+						boolean result = ws.remove(ar);
+						String message="삭제 실패";
+						if(result) {
+							message="삭제 성공";
+						}
+						wv.view(message);	
+						
+						//ws.remove(ar);
+						// 나는 추가적으로 삭제가 되었는지 알고 싶음 => weatherservice(boolean타입 생성)
+						
+					}else {
+						
+						check=!check;
+						break;
+						
+						
+					}
+				}
+				
+			}
+	
+	
 	}
 	
-}
+
--- src/org/lang/list_ex1/WeatherService.java
+++ src/org/lang/list_ex1/WeatherService.java
@@ -1,10 +1,9 @@
 package org.lang.list_ex1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-import org.lang.String.ex1.WorkerDTO;
-import org.object1.ex1.Student;
 
 public class WeatherService {
 	
@@ -25,11 +24,12 @@ public class WeatherService {
 	//find 메서드
 	//도시명을 입력받아서 리스트에서 검색 리턴
 	private StringBuffer sb;
-	
-	
+	private Scanner sc;
 	public WeatherService() {
 		
 		sb= new StringBuffer();
+		sc= new Scanner(System.in);
+		
 		sb.append("seoul, 30.2, 70%, 맑음,");
 		sb.append("Daegu- 38.9- 90%- 흐림-");
 		sb.append("jeju, 12.1, 10, 눈,");
@@ -38,20 +38,28 @@ public class WeatherService {
 	}
 	
 			public void init (ArrayList<CityDTO> ar) {
-					String info =sb.toString(); // sb를 담은 주소를 문자열로 반환
-					String newinfo = info.replace("-", ","); // 바꿀 것 
-					StringTokenizer st = new StringTokenizer (newinfo,"-");
+					String data =sb.toString(); // sb를 담은 주소를 문자열로 반환
+					System.out.println(data);
+					data = data.replace("-", ","); // 바꿀 것 
 					
+					StringTokenizer st = new StringTokenizer (data,",");
+					
+					while (st.hasMoreTokens()) {
 					
 						CityDTO cityDTO = new CityDTO ();
+						cityDTO.setName(st.nextToken());
+						cityDTO.setGion(Double.parseDouble(st.nextToken())); //parseDouble -문자열을 더블타입으로 변환
+						cityDTO.setHum(Integer.parseInt(st.nextToken()));
+						cityDTO.setStatus(st.nextToken());
+						ar.add(cityDTO);
 						
 						
-						
+					}
 					
 			}
 		
 		
-				public void add(ArrayList<CityDTO> ar) {
+				public boolean add(ArrayList<CityDTO> ar) {
 		
 					//add 메서드 
 					//=city정보를 입력받아서 기존에 정보에 추가
@@ -59,54 +67,99 @@ public class WeatherService {
 
 					//1.ArrayList <CityDTO> 받아와서 추가
 					
+//					CityDTO cityDTO = new CityDTO();
+//					cityDTO.setName("test");
+//					cityDTO.setGion(20.3);
+//					cityDTO.setHum(80);
+//					cityDTO.setStatus("rain");
+					
+//					ar.add(cityDTO); // 받아온 주소엔 추가된 내용은 넣으면됨 
+					
 					CityDTO cityDTO = new CityDTO();
-					cityDTO.setName("test");
-					cityDTO.setGion(20.3);
-					cityDTO.setHum(80);
-					cityDTO.setStatus("rain");
 					
-					ar.add(cityDTO); // 받아온 주소엔 추가된 내용은 넣으면됨 
+					System.out.println("도시명 입력");
+					cityDTO.setName(sc.next());
+					System.out.println("기온 입력");
+					cityDTO.setGion(sc.nextDouble());
+					System.out.println("습도 입력");
+					cityDTO.setHum(sc.nextInt());
+					System.out.println("날씨 입력");
+					cityDTO.setStatus(sc.next());
+					return ar.add(cityDTO);
 					
-	}
+					
+				} //add 끝 
 	
-				public void remove() {
+				public boolean remove(ArrayList<CityDTO> ar) {
 					//remove 메서드
 					//도시명을 입력받아서 리스트에서 삭제 
-
-		
+						System.out.println("삭제 하려는 도시명 입력");
+						String name = sc.next().toUpperCase(); // 넥스트를 입력하는 문자열을 대문자로 만들면서 데이터를 집어 넣음
+						boolean r =false;
+						
+						//전통 for
+						
+						
+						for(int i=0;i<ar.size();i++) {
+							if(name.equals(ar.get(i).getName().toUpperCase())) { //CityDTO의 이름을 꺼내와
+								ar.remove(i);
+								r= !r;
+								break;
+							}
+						}
+						return r;
+							
+							
+							// 향상된 for문  ,인덱스번호가 아닌 메서드를 사용
+//						for(CityDTO cityDTO: ar) {
+//							if(name.equals(cityDTO.getName())) {
+//								ar.remove(cityDTO);
+//								break;
+//							}
+								
+//							}
+							
+							
 	}
+						
 
-				public void find(ArrayList<CityDTO> ar) {
+				public CityDTO find(ArrayList<CityDTO> ar) {
 	
 					//find 메서드
 					//도시명을 입력받아서 리스트에서 검색 리턴
 						
+//					CityDTO cityDTO =null; 
+//					return cityDTO; 쓰고 시작하면 에러를 제외하고 사용할 수 있음/ 에러 난 곳을 찾으려고 하면 해당된 곳이 에러라고 안뜨는 경우도 있다함
+					
+					CityDTO cityDTO =null;
 						System.out.println(" 도시명을 입력하시오 ");
-						int city =sc.nextInt(); 
+						String name =sc.next().toUpperCase(); 
 							
-						for (int i=0; i<; i++) {   
+					
+						//향상된 for
+						
+						for(CityDTO cityDTO2 : ar) {
 							
-							if() {
-								
-								
+							if(name.equals(cityDTO2.getName().toLowerCase())) {
+								cityDTO = cityDTO2;
 								break;
-								
-							}
-								
 						}
-									return city;	 //찾으면 student 못찾으면 null		
+				
+					
+									
 						 
 				}
 				
 				
-				}
-	
+				
+				return cityDTO;	 //찾으면 못찾으면 null		
+
+
 }
 
 
 
-
-
+}
 
 
 
